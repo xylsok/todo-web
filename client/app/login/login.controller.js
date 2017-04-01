9b9ae4da53d93e0fc0d636a1f1522efa29c732b3
xylsok <xyls_ok@126.com>
2017-04-01 23:52:59 +0800

    login

--- client/app/login/login.controller.js
+++ client/app/login/login.controller.js
@@ -2,7 +2,12 @@
 (function() {
 
 function MainController($scope, $http,$resource) {
-
+	$scope.login={
+		loginState:true,
+		_show:function(){
+			$scope.login.loginState=!$scope.login.loginState;
+		}
+	}
 }
 angular.module('lawqWebApp')
   .controller('LoingController', MainController);
