8c54fa388ce58208215ebce09eb827181055bb98
张振峰 <xyls_ok@126.com>
2017-07-28 17:42:52 +0800

    test

--- client/app/login/login.controller.js
+++ client/app/login/login.controller.js
@@ -3,23 +3,25 @@
 	function Controller($scope, $http, Auth, $state) {
 		$scope.user = {};
 		$scope.login = {
-			msg:'',
+			msg: '',
 			_login: function () {
 				$http.post('/api/user/login', $scope.user).success(function (x) {
 					if (x) {
 						Auth.setUser(x);
 						$state.go('main');
-					}else{
-						$scope.login.msg='用户名或密码错误！';
+					} else {
+						$scope.login.msg = '用户名或密码错误！';
+						alert($scope.login.msg);
 					}
-				}).error(function(e){
-					$scope.login.msg='服务器发生错误。';
+				}).error(function (e) {
+					$scope.login.msg = '服务器发生错误。';
+					alert($scope.login.msg);
 				})
 			},
-			_login2:function(){
-				$scope.user.userName='ceshi';
-				$scope.user.nickName='测试';
-				$scope.user.password='111111';
+			_login2: function () {
+				$scope.user.userName = 'ceshi';
+				$scope.user.nickName = '测试';
+				$scope.user.password = '111111';
 				$scope.login._login();
 			}
 		}
