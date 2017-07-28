e8010af1d1f6c9720da05b0fdc9f53d21eb630db
张振峰 <xyls_ok@126.com>
2017-07-28 14:52:04 +0800

    tes

--- client/app/finish/finish.controller.js
+++ client/app/finish/finish.controller.js
@@ -1,10 +1,21 @@
 'use strict';
-(function() {
+(function () {
+	function Controller($scope, $state, $resource, $http, Auth) {
+		var user = Auth.getUser();
+		if (user && user.userName) {
+			$scope.finish = $resource('/api/memo/getfinish?username=' + user.userName).query();
+		} else {
+			$state.go('login');
+		}
+		$scope.delMemo = function (s) {
+			$http.delete('/api/memo/delete/'+ s.id).success(function () {
+				_.remove($scope.finish,function (y) {
+					return y.id == s.id;
+				})
+			})
+		}
+	}
 
-function Controller($scope, $http,$resource,$sce) {
-
-}
-angular.module('todoWebApp')
-  .controller('FinishController', Controller);
-
+	angular.module('todoWebApp')
+		.controller('FinishController', Controller);
 })();
