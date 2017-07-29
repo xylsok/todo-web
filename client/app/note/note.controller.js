63ba986189882e0ec5003f725b7f3c36ba9652e8
张振峰 <xyls_ok@126.com>
2017-07-29 12:44:18 +0800

    test

--- client/app/note/note.controller.js
+++ client/app/note/note.controller.js
@@ -1,7 +1,21 @@
 'use strict';
 (function() {
 
-function Controller($scope, $http,$resource,$sce) {
+function Controller($scope, $http,$resource,Auth) {
+	$scope.user = Auth.getUser();
+	$scope.note ={};
+	$http.post('/api/note/add',$scope.note).success(function(x){
+
+	})
+	$scope.core={
+		notes:[],
+		_init:function(){
+			$http.get('/api/note/get/'+$scope.user.userName).success(function(x){
+				$scope.core.notes=x;
+			})
+		}
+	}
+	$scope.core._init();
 
 }
 angular.module('todoWebApp')
